c168bf51338fbfa2fc8984142facab6352dc956f
cabreralcui <cabrera.lcui@gmail.com>
2016-11-06 14:45:32 -0600

    Added tyler's code for GUI Builder

---
+++ gui_builder.java
@@ -0,0 +1,21 @@
+package frontend;
+
+import java.util.ArrayList;
+import javafx.scene.control.*;
+
+
+public class GUI_Builder
+{
+    public GUI_Builder(){}
+    
+    private TitledPane buildTitledPane(String tag, ArrayList entries)
+        {
+            TitledPane titledPane = new TitledPane();
+            
+            
+            
+            return titledPane;
+        }   
+    
+    
+}
